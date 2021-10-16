a9ab3053d444c132803ab41fa8c21d7546bd1eca
JonatanGarcia1746624 <jonyjony19955@hotmail.com>
2021-10-16 16:30:43 -0500

    SE TERMINO LA OPCION 1

--- venta_De_Llantas.py
+++ venta_De_Llantas.py
@@ -10,7 +10,7 @@ Salida1 = int(0)
 while Salida1 == int(0):
     venta_Info = namedtuple("Venta","Fecha_V,Desc_V,Cant_V,Precio_V")
 
-    print(f'BASE DE DATOS "LLANTAS DON ELIAZAR"')
+    print(f'\nBASE DE DATOS "LLANTAS DON ELIAZAR"')
     print(f'1.REGISTRAR UNA VENTA')
     print(f'2.CONSULTAR UNA VENTA')
     print(f'3.SALIR')
@@ -30,20 +30,38 @@ while Salida1 == int(0):
         else:
             Folio = num_Folio
             Salida2 = int(0)
+            
             while Salida2 == int(0):
+                print("AGREGAR ARTICULO")
                 fecha_venta = str(datetime.date.today())
                 desc_Articulo = str(input("DESCRIPCIÓN DEL ARTICULO: "))
-                cantidad_Articulo = int(input("CANTIDAD: "))
+                cantidad_Articulo = float(input("CANTIDAD: "))
                 precio = float(input("PRECIO DEL ARTICULO: "))
                 datos_Venta = venta_Info(fecha_venta, desc_Articulo,cantidad_Articulo,precio)
                 articulo_list.append(datos_Venta)
-                print(f'DESEA AGREGAR OTRO ARTICULO SI[0], NO[1]')
+                print(f'\nDESEA AGREGAR OTRO ARTICULO SI[0], NO[1]')
                 exit = int(input("OPCIÓN: "))
                 Salida2 = exit
+                print()
             articulo_list2 = articulo_list.copy()
             venta_list[num_Folio] = articulo_list2
             del(articulo_list[:])
-            print(f'\n***VENTA REGISTRADA CON EXITO***')
-            print(f'\n{venta_list}\n')
+            print(f'\n***VENTA REGISTRADA CON EXITO***\n')
+            pos = venta_list[num_Folio]
+            print(f'FECHA DE VENTA: {fecha_venta}')
+            subtotal = int(0)
+            for elemento in pos:
+                importe = elemento.Cant_V * elemento.Precio_V
+                print(f'DESCRIPCIÓN ARTICULO: {elemento.Desc_V}')
+                print(f'CANTIDAD: {elemento.Cant_V}')
+                print(f'PRECIO UNITARIO: {elemento.Precio_V}')
+                print(f'IMPORTE: {importe}')
+                subtotal += importe
+                print()
+            print(f'SUBTOTAL: {subtotal}')
+            print(f'IVA(16%): {subtotal * 0.16}')
+            print(f'TOTAL: {(subtotal) + (subtotal * 0.16)}')
             Salida1 = int(0)
-            
+    else:
+        if op == "2":
+            print()
