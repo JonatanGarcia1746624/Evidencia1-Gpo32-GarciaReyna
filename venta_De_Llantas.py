3bc4d5b67b07a0669261166fe4fe2d3bbfa32382
JonatanGarcia1746624 <jonyjony19955@hotmail.com>
2021-10-16 17:00:08 -0500

    CODIGO TERMINADO

--- venta_De_Llantas.py
+++ venta_De_Llantas.py
@@ -64,4 +64,25 @@ while Salida1 == int(0):
             Salida1 = int(0)
     else:
         if op == "2":
-            print()
+            print(f'\nCONSULTAR UNA VENTA')
+            print(f'INGRESE EL FOLIO DE LA VENTA QUE DESEA CONSULTAR')
+            num_Venta = int(input("Numero de Folio: "))
+            if num_Venta in venta_list.keys():
+                pos = venta_list[num_Venta]
+                print("\n***FOLIO ENCONTRADO***\n")
+                print(f'FECHA: {pos[0].Fecha_V}')
+                subtotal = int(0)
+                for elemento in pos:
+                    importe = elemento.Cant_V * elemento.Precio_V
+                    print(f'DESCRIPCIÓN ARTICULO: {elemento.Desc_V}')
+                    print(f'CANTIDAD: {elemento.Cant_V}')
+                    print(f'PRECIO UNITARIO: {elemento.Precio_V}')
+                    print(f'IMPORTE: {importe}')
+                    subtotal += importe
+                    print()
+                print(f'SUBTOTAL: {subtotal}')
+                print(f'IVA(16%): {subtotal * 0.16}')
+                print(f'TOTAL: {(subtotal) + (subtotal * 0.16)}')
+            else:
+                print("\n¡¡¡FOLIO NO ENCONTRADO!!!")
+        
