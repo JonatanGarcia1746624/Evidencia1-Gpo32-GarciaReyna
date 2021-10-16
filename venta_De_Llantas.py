4e9fdb4ed5b02b4d4535307c378e955aa25b6214
JonatanGarcia1746624 <jonyjony19955@hotmail.com>
2021-10-16 00:58:12 -0500

    SE AGREGO EL MEUNO Y LA SELECCION DEL USUARIO

--- venta_De_Llantas.py
+++ venta_De_Llantas.py
@@ -1,5 +1,14 @@
-#Modulos que se utilizaran 
+#MODULOS QUE SE UTILIZARAN
 from collections import namedtuple
 import datetime
 import time
 
+#MENU
+print(f'LLANTAS EL "TÍO JUAN"')
+print(f'1.REGISTRAR UNA VENTA')
+print(f'2.CONSULTAR UNA VENTA')
+print(f'3.SALIR\n')
+
+#OPCIÓN DEL USUARIO
+print(f'SELECCIONE EL NUMERO CORRESPONDINTE DE LA ACCIÓN A REALIZAR')
+op = input("OPCIÓN: ")
