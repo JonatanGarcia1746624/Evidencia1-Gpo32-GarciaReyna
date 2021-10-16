d7f71fb358889fead1ee20dec546a986f06aa17b
JonatanGarcia1746624 <jonyjony19955@hotmail.com>
2021-10-16 01:05:16 -0500

    SE AGREGO LA LISTA EL SIWITCH DE SALIDA DEL WHILE

--- venta_De_Llantas.py
+++ venta_De_Llantas.py
@@ -3,12 +3,22 @@ from collections import namedtuple
 import datetime
 import time
 
-#MENU
-print(f'LLANTAS EL "TÍO JUAN"')
-print(f'1.REGISTRAR UNA VENTA')
-print(f'2.CONSULTAR UNA VENTA')
-print(f'3.SALIR\n')
+#LISTA DONDE SE REGISTRAN LAS VENTAS
+ventas_List = {}
 
-#OPCIÓN DEL USUARIO
-print(f'SELECCIONE EL NUMERO CORRESPONDINTE DE LA ACCIÓN A REALIZAR')
-op = input("OPCIÓN: ")
+#SWITCH PARA SALIR DEL WHILE
+
+Salida = int(0)
+while Salida == int(0):
+    #MENU
+    print(f'LLANTAS EL "TÍO JUAN"')
+    print(f'1.REGISTRAR UNA VENTA')
+    print(f'2.CONSULTAR UNA VENTA')
+    print(f'3.SALIR\n')
+
+    #OPCIÓN DEL USUARIO
+    print(f'SELECCIONE EL NUMERO CORRESPONDINTE DE LA ACCIÓN A REALIZAR')
+    op = input("OPCIÓN: ")
+
+    if op == '1':
+        folio = int()
