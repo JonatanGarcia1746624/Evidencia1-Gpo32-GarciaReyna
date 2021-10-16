77e0810495a53175cff4c83d432a73269a247290
JonatanGarcia1746624 <jonyjony19955@hotmail.com>
2021-10-16 14:57:21 -0500

    SE MODIFICO TODO EL CODIGO EL ANTERIOR NO FUNCIONABA

--- venta_De_Llantas.py
+++ venta_De_Llantas.py
@@ -1,52 +1,49 @@
-#MODULOS QUE SE UTILIZARAN
 from collections import namedtuple
 import datetime
 import time
 
-#FUNCIONES A UTILIZAR
-#1.CONTAR ELEMENTOS EN LA LISTA
-def contar_ElementosEnLaLista(lista):
-    numElementos = len(lista)
-    return numElementos
 
+articulo_list = []
+venta_list = {}
 
-#LISTA DE LAS VENTAS
-ventas_List = {}
-
-#SWITCH PARA SALIR DEL WHILE
 Salida1 = int(0)
-salida2 = int(0)
 while Salida1 == int(0):
-    #FORMATO DE LA LISTA DE VENTAS
-    venta_Info = namedtuple("VENTA","infoVenta")
+    venta_Info = namedtuple("Venta","Fecha_V,Desc_V,Cant_V,Precio_V")
 
-    #REGISTRO DE ARTICULOS VENDIOS
-    datos_Articulo = namedtuple("ARTICULO","DescArticulo,Precio,fecha_venta")
-    #MENU
-    print(f'LLANTAS EL "TÍO JUAN"')
+    print(f'BASE DE DATOS "LLANTAS DON ELIAZAR"')
     print(f'1.REGISTRAR UNA VENTA')
     print(f'2.CONSULTAR UNA VENTA')
-    print(f'3.SALIR\n')
+    print(f'3.SALIR')
+    op = input("OPCIÓN:")
+    print()
 
-    #OPCIÓN DEL USUARIO
-    print(f'SELECCIONE EL NUMERO CORRESPONDINTE DE LA ACCIÓN A REALIZAR')
-    op = input("OPCIÓN: ")
+    if op == "1":
+        Folio = int()
+        print("DIGITE EL NUMERO DE FOLIO DE VENTA")
+        print("SI DIGITA UN FOLIO NO EXISTENTE NO SE REGISTRARA")
+        print("EL SISTEMA MARCARA ERROR Y VOLVERA AL MENU")
+        num_Folio = int(input("FOLIO: "))
+        print()
 
-    if op == '1':
-        folios_ActualesEnLista = contar_ElementosEnLaLista(ventas_List)
-        folio = folios_ActualesEnLista + 1
-        articulos_List = ()
-        while salida2 == int(0):
-            print(f'\nPORFAVOR INGRESE EL ARTICULO VENDIDO')
-            desc_Articulo = str(input("DESCRIPCIÓN DEL ARTICULO: "))
-            precio = float(input("PRECIO DEL ARTICULO: "))
-            fecha_venta = str(datetime.date.today())
-            informacion_Articulos = datos_Articulo(desc_Articulo.upper(),precio,fecha_venta)
-            articulos_List.append(informacion_Articulos)
-            informacion_Venta = articulos_List
-            check_Salida = int(input("DESEA AGREGAR OTRO ARTICULO SI[0], NO[1]: "))
-            salida2 = check_Salida
-        ventas_List[folio] = informacion_Venta
-        print(f'\n"***VENTA REGISTRADA CON EXITO***"')
-        print(f'\n{ventas_List}\n')
-    Salida1 = int(0)
+        if num_Folio in venta_list.keys():
+            print("¡¡¡FOLIO YA EXISTENTE!!!\n")
+        else:
+            Folio = num_Folio
+            Salida2 = int(0)
+            while Salida2 == int(0):
+                fecha_venta = str(datetime.date.today())
+                desc_Articulo = str(input("DESCRIPCIÓN DEL ARTICULO: "))
+                cantidad_Articulo = int(input("CANTIDAD: "))
+                precio = float(input("PRECIO DEL ARTICULO: "))
+                datos_Venta = venta_Info(fecha_venta, desc_Articulo,cantidad_Articulo,precio)
+                articulo_list.append(datos_Venta)
+                print(f'DESEA AGREGAR OTRO ARTICULO SI[0], NO[1]')
+                exit = int(input("OPCIÓN: "))
+                Salida2 = exit
+            articulo_list2 = articulo_list.copy()
+            venta_list[num_Folio] = articulo_list2
+            del(articulo_list[:])
+            print(f'\n***VENTA REGISTRADA CON EXITO***')
+            print(f'\n{venta_list}\n')
+            Salida1 = int(0)
+            
