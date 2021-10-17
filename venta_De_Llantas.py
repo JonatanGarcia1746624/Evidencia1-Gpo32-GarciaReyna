08145224c11cd8a8d5fd3ea20f81c929a31f0340
JonatanGarcia1746624 <jonyjony19955@hotmail.com>
2021-10-16 20:41:55 -0500

    SE AGREGARON COMENTARIOS PARA SU MEJOR COMPRENDIMIENTO

--- venta_De_Llantas.py
+++ venta_De_Llantas.py
@@ -1,23 +1,34 @@
+
+#IMPORTACIONES DE LAS LIBRERIAS A UTILIZAR
 from collections import namedtuple
 import datetime
 import time
 
-
+#CREACIÓN DE UNA LISTA Y UN DICCIONARIO QUE SE UTILIZARA 
+#POSTERIORMENTE PARA TRABAJAR CON LAS TUPLAS ANIDADAS
 articulo_list = []
 venta_list = {}
 
 Salida1 = int(0)
 while Salida1 == int(0):
+    #SUBCLASE DE LISTA CON LOS CAMPOS CORRESPONDIENTES PARA SU
+    #POSTERIOR USO AL REGISTRAR LAS VENTAS
     venta_Info = namedtuple("Venta","Fecha_V,Desc_V,Cant_V,Precio_V")
 
+    #UN MENU SENCILLO QUE NOS SERVIRA PARA QUE EL USUARIO 
+    #SELECCIONE LA ACCIÓN A REALIZAR
     print(f'\nBASE DE DATOS "LLANTAS DON ELIAZAR"')
     print(f'1.REGISTRAR UNA VENTA')
     print(f'2.CONSULTAR UNA VENTA')
     print(f'3.SALIR')
     op = input("OPCIÓN:")
+    
     print()
 
     if op == "1":
+        #CREACION DE UN NUMERO DE FOLIO UNICO AQUI EL USUARIO
+        #DEBERA DIGITAR UN NUMERO Y POSTERIORMENTE EL PROGRAMA
+        #LE AVISARA SI EL NUMERO DE FOLIO YA EXISTE
         Folio = int()
         print("DIGITE EL NUMERO DE FOLIO DE VENTA")
         print("SI DIGITA UN FOLIO NO EXISTENTE NO SE REGISTRARA")
@@ -32,6 +43,10 @@ while Salida1 == int(0):
             Salida2 = int(0)
             
             while Salida2 == int(0):
+                #AQUI ES DONDE OCURRE LA MAGIA PUES COMO SE VIO ANTES
+                #SE HARA USO DE LAS LISTAS ANIDADAS Y SE GUARDARAN
+                #LOS DATOS RECIBIDOS CON UN FORMATO EN EL CUAL CADA
+                #DATO CONTENGA SU ESPACIO CORRESPONDIENTE
                 print("AGREGAR ARTICULO")
                 fecha_venta = str(datetime.date.today())
                 desc_Articulo = str(input("DESCRIPCIÓN DEL ARTICULO: "))
@@ -39,14 +54,30 @@ while Salida1 == int(0):
                 precio = float(input("PRECIO DEL ARTICULO: "))
                 datos_Venta = venta_Info(fecha_venta, desc_Articulo,cantidad_Articulo,precio)
                 articulo_list.append(datos_Venta)
+                #AQUI SE LE PREGUNTA AL USUARIO SI DESEA AGREGAR OTRO ARTICULO
+                #DE SER ASÍ EL CILCO SE REPITE PERO EN CAMBIO SI NO ES ASÍ
+                #EL CODIGO CONTINUARA SALIENDO DE UN WHILE EN EL QUE ESTA 
+                #AHORA QUE EL USUARIO ESTA FUERA DEK WHILE LO QUE SIGUE ES
+                #ESTA PARTE ES IMPORTANTE YA QUE AQUI LO QUE HACE EL PROGRAMA
+                #ES GUARDAR TODOS LOS DATOS EN UNA LISTA Y LUEGO LES HACE UNA
+                #COPIA QUE SE REGISTRARA EN EL DICCIONARIO CON SU FOLIO UNICO
                 print(f'\nDESEA AGREGAR OTRO ARTICULO SI[0], NO[1]')
                 exit = int(input("OPCIÓN: "))
                 Salida2 = exit
                 print()
             articulo_list2 = articulo_list.copy()
             venta_list[num_Folio] = articulo_list2
+            #PARA QUE LOS DATOS NO SE QUEDEN GUARDADOS EN LA LISTA ANTERIOR
+            #AQUI LO QUE SE HACE ES USAR LA FUNCIÓN "DEL" QUE BORRA MEDIANTE
+            #NOTACIÓN DE SLICING TODOS LOS DATOS DE LA LISTA PARA DEJARLA
+            #VACIA PARA SU POSTERIOR USO Y SE CONFIRMA EL REGISTRO DE VENTA
             del(articulo_list[:])
             print(f'\n***VENTA REGISTRADA CON EXITO***\n')
+            #AQUI SE GUARDA LA POSICION DEL ANTERIOR REGISTRO PARA
+            #DESPUES IMPRIMIR EL TICKET DE VENTA EL CUAL SE IMPRIME
+            #MEDIANTE LOS METODOS ANTERIORES QUE TENIAMOS CON SU
+            #RESPECTIVO NOMBRE Y SE HACEN LAS RESPECTIVAS OPERACIONES
+            #PARA SACAR SL SUBTOTAL, IVA Y EL TOTAL
             pos = venta_list[num_Folio]
             print(f'FECHA DE VENTA: {fecha_venta}')
             subtotal = int(0)
@@ -63,6 +94,12 @@ while Salida1 == int(0):
             print(f'TOTAL: {(subtotal) + (subtotal * 0.16)}')
             Salida1 = int(0)
     else:
+        #AHORA QUE SI EL USUARIO ESCOGIO CONSULTAR UN REGISTRO 
+        #LO QUE SE HACE ES QUE MEDIANTE EL FOLIO QUE AQUI FUNCIONA
+        #COMO UNA LLAVE DEL DICCIONARIO BUSCA EL REGISTRO EN CASO
+        #DE EXISTIR E IMPRIME LOS DATOS USANDO EL MISMO METODO QUE
+        #SE USO ANTERIORMENTE Y EN CASO DE NO ENCONTRAR EL FOLIO
+        #EL PROGRAMA LE AVISA AL USUARIO Y REGRESA AL MENU
         if op == "2":
             print(f'\nCONSULTAR UNA VENTA')
             print(f'INGRESE EL FOLIO DE LA VENTA QUE DESEA CONSULTAR')
@@ -86,6 +123,6 @@ while Salida1 == int(0):
             else:
                 print("\n¡¡¡FOLIO NO ENCONTRADO!!!")
         else:
+            #EN ESTA OPCIÓN LO UNICO QUE PASA ES QUE EL PROGRAMA FINALIZA
             if op == "3":
                 Salida1 = int(1)
-        
